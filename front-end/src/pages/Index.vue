4dabca57cddd9a652471deb384765aeca6f7fad1
BrentMosier <brentmosier1@gmail.com>
2022-08-02 15:46:21 -0400

    first commit

---
+++ front-end/src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>Pick photo</div>
+    <div>Upload</div>
+    <div>List of cards</div>
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    /> -->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
